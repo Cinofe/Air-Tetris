85c2a50eb30ca47b94b3cdfb69e9b860605cd9af
= <=>
2022-05-31 21:43:38 +0900

    Tetris

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ class Main:
         except Exception as e:
             self.Error('stream Connect Error : ', e)
 
-        cap = cv2.VideoCapture(cv2.CAP_DSHOW+0)
+        cap = cv2.VideoCapture(cv2.CAP_DSHOW,cv2.CAP_ANY)
         ret, _ = cap.read()
         if ret == False:
             print('cam error')
