5647f091a0726c5d098c5d7dc7859a9f75ce7a2d
= <=>
2022-05-31 20:30:17 +0900

    Tetris

--- menu.py
+++ menu.py
@@ -22,10 +22,10 @@ class Menu:
         self.playText = self.font.render("PLAY", True, (255,255,255))
         self.quitText = self.font.render("QUIT", True, (255,255,255))
 
-        self.rect_UI = pg.Rect(self.__hw-115,self.__hh,415,224)
-        self.rect_levelBox = pg.Rect((self.__hw-165,self.__hh-240,315,70))
-        self.rect_playBox = pg.Rect((self.__hw-165,self.__hh-167,315,70))
-        self.rect_quitBox = pg.Rect((self.__hw-165,self.__hh-94,315,70))
+        self.rect_UI = pg.Rect(self.__hw-115,self.__hh+50,415,224)
+        self.rect_levelBox = pg.Rect((self.__hw-165,self.__hh-190,315,70))
+        self.rect_playBox = pg.Rect((self.__hw-165,self.__hh-117,315,70))
+        self.rect_quitBox = pg.Rect((self.__hw-165,self.__hh-44,315,70))
 
         self.selections = [(self.rect_levelBox, self.levelText),
                             (self.rect_playBox, self.playText),
