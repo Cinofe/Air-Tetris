e23ffc35a074a5fe1ab390d653d24ed731817c17
cinofe <jeraru5178@gmail.com>
2022-06-06 21:42:30 +0900

    Tetris

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ class Main:
                 ## 모션으로 조정
                 self.Get_motion()
                 if any(self.hand_box):
-                    cv2.rectangle(self.frame,self.hand_box,(0,0,0),1)
+                    cv2.rectangle(self.frame,self.hand_box,(0,0,0),3)
                 G.call_image(self.frame)
                 # Left, Right는 처음 입력 받고 다음에서 같은 데이터가 오면 0.7초 대기 후, 
                 # 0.2초마다 Move_Left 실행
