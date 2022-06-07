86608a37f0428239d25fb7d95dea398141cf8c4a
cinofe <jeraru5178@gmail.com>
2022-06-07 10:31:04 +0900

    Tetris

--- main.py
+++ main.py
@@ -154,9 +154,9 @@ class Main:
                 if t.time() - d_stime >= down_delay:
                     G.Move_Down()
                     d_stime = t.time()
-                if t.time() - u_start >= up_block_delay:
+                if t.time() - u_stime >= up_block_delay:
                     G.Line_Plus()
-                    u_start = t.time()
+                    u_stime = t.time()
                 if G.GAME_OVER():
                     if self.Best_Score < G.Score:
                         self.Best_Score = G.Score
