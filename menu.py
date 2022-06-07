c8277f432867ddaa9045da0f74186eb155ef90c3
cinofe <jeraru5178@gmail.com>
2022-06-07 10:14:48 +0900

    Tetris

--- menu.py
+++ menu.py
@@ -24,9 +24,9 @@ class Menu:
         self.playText = self.font.render("PLAY", True, (255,255,255))
         self.quitText = self.font.render("QUIT", True, (255,255,255))
 
-        self.rect_levelBox = pg.Rect((self.__hw-160,self.__hh-160,315,70))
-        self.rect_playBox = pg.Rect((self.__hw-160,self.__hh-27,315,70))
-        self.rect_quitBox = pg.Rect((self.__hw-160,self.__hh+115,315,70))
+        self.rect_levelBox = pg.Rect((self.__hw-160,self.__hh-140,315,70))
+        self.rect_playBox = pg.Rect((self.__hw-160,self.__hh-7,315,70))
+        self.rect_quitBox = pg.Rect((self.__hw-160,self.__hh+135,315,70))
 
         self.selections = [(self.rect_levelBox, self.levelText),
                             (self.rect_playBox, self.playText),
