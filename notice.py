4175b159f6d63bec4e9eac098bbbc18d16aa3ded
cinofe <jeraru5178@gmail.com>
2022-06-07 10:09:01 +0900

    Tetris

--- notice.py
+++ notice.py
@@ -15,19 +15,10 @@ class Notice:
         self.explanations = [
             "플레이 방법",
             "오른손 사용 필수",
-            "손가락을 모두 붙인 상태에서",
-            "손바닥이 정면을 보게하여",
-            "손끝을 위로 향하게 하면",
-            "블럭이 회전",
-            "손등이 정면을 보게하여",
-            "손끝을 아래로 향하게 하면",
-            "블럭이 즉시 바닥으로 내려감",
-            "손바닥이 정면을 보게하여",
-            "손끝을 오른쪽으로 향하게 하면",
-            "블럭이 오른쪽으로 이동",
-            "손등이 정면을 보게하여",
-            "손끝이 왼쪽을 향하게 하면",
-            "블럭이 왼쪽으로 이동",
+            "오른쪽 이동 : 손바닥이 정면을 보게하여 손끝이 오른쪽",
+            "왼쪽   이동 : 손등이 정면을 보게하여 손끝이 왼쪽",
+            "블럭   회전 : 손바닥이 정면을 보게하여 손끝이 위로",
+            "블럭 내리기 : 손등이 정면을 보게하여 손끝이 아래로",
             "모두 읽었다면 가운데 버튼 누르기"
         ]
     def update(self):
@@ -38,7 +29,7 @@ class Notice:
     def draw_text(self):
         self.screen.blit(pg.font.SysFont('undotum',100).render(self.title, True, (255,255,255)),(self.__hw,150))
         for i, explanation in enumerate(self.explanations):
-            self.screen.blit(pg.font.SysFont('undotum',50).render(explanation, True, (255,255,255)),(self.__hw-165,240+(80*(i+1))))
+            self.screen.blit(pg.font.SysFont('undotum',40).render(explanation, True, (255,255,255)),(self.__hw-165,240+(80*(i+1))))
 
     def run(self):
         done = False
